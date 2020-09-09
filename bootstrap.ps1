47b81a98e46ccb75b79b215030debb59a6ff2b19
Jonathon Frame <42950310+jon-frame@users.noreply.github.com>
2020-09-09 10:13:07 +1000

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,7 @@
+## To do:
+
+# Attach the secondary disk and initialise it
+# Write content to disk for later retrieval
+# Dismount the Disk
+# Use Azure PS Module to detach the disk so that we can use it with our bastion lab instance
+# Successfully exit script
