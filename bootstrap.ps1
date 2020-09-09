b2294e89060901fae9e28ea42b48fac2eb25ae5b
Jonathon Frame <42950310+jon-frame@users.noreply.github.com>
2020-09-09 12:47:50 +1000

    Update bootstrap.ps1
    Added content to mount the disk and drop an image on it

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,10 +1,23 @@
-## To do:
+ ## To do:
 
-# Attach the secondary disk and initialise it
+# Mount the secondary disk (It will be a RAW disk)
+$NewPartition = Get-Disk |?{$_.PartitionStyle -eq "RAW"} |  Initialize-Disk -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume
+
 # Write content to disk for later retrieval
+Invoke-WebRequest https://github.com/jon-frame/573_Add_Existing_Data_Disk_in_Azure/raw/master/acg_logo.jpg -OutFile ($NewPartition.DriveLetter+"://acg_logo.jpg")
+
 # Dismount the Disk
+$ExtraDisk = Get-Disk |?{$_.PartitionStyle -eq "GPT"} | Set-Disk -IsOffline $true
+
+# Get VM Metadata so we can find and detach the disk
+$Metadata = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri http://169.254.169.254/metadata/instance?api-version=2020-06-01
+
+
+
 # Use Azure PS Module to detach the disk so that we can use it with our bastion lab instance
 # Successfully exit script
+
+
 
 if (1 -eq 1) {
 # everything went according to plan
@@ -18,3 +31,4 @@ if (1 -eq 1) {
 }
 
 EXIT $LastExitCode
+ 
