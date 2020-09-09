7eab3f985763c055942396da570d9559efec2246
Jonathon Frame <42950310+jon-frame@users.noreply.github.com>
2020-09-09 10:35:46 +1000

    Update bootstrap.ps1
    Testing remotely-hosted config data with valid exit code

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -5,3 +5,18 @@
 # Dismount the Disk
 # Use Azure PS Module to detach the disk so that we can use it with our bastion lab instance
 # Successfully exit script
+
+if (1 = 1) {
+# everything went according to plan
+
+    $LastExitCode = 0
+
+}
+
+else{  
+# something didn't work right, so the deployment should fail
+
+    $LastExitCode = 1
+}
+
+EXIT $LastExitCode
